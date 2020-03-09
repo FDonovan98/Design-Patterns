e22c7ec460818135d921e6eaba0a82551a005536
HDonovan96 <harry.bd11@gmail.com>
2020-03-09 16:12:50 +0000

    Switched to generic object factory rather than generic monobehaviour factory

---
+++ Assets/Script/GenericObjectFactory.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GenericObjectFactory<T> : MonoBehaviour where T : Object
+{
+    [SerializeField]
+    private T prefab;
+
+    public T GetNewInstance()
+    {
+        return Instantiate(prefab);
+    }
+}
