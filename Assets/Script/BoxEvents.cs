46790281c78bfc9c8e81478d4cf74a5c0cb31f0a
HDonovan96 <harry.bd11@gmail.com>
2020-03-09 19:34:48 +0000

    Added in tutorials scripts

---
+++ Assets/Script/BoxEvents.cs
@@ -0,0 +1,34 @@
+﻿// Code from: https://www.habrador.com/tutorials/programming-patterns/3-observer-pattern/
+
+//Events
+public abstract class BoxEvents
+{
+    public abstract float GetJumpForce();
+}
+
+
+public class JumpLittle : BoxEvents
+{
+    public override float GetJumpForce()
+    {
+        return 30f;
+    }
+}
+
+
+public class JumpMedium : BoxEvents
+{
+    public override float GetJumpForce()
+    {
+        return 60f;
+    }
+}
+
+
+public class JumpHigh : BoxEvents
+{
+    public override float GetJumpForce()
+    {
+        return 90f;
+    }
+}
---
+++ Assets/Script/Subject.cs
@@ -0,0 +1,32 @@
+﻿// Code from: https://www.habrador.com/tutorials/programming-patterns/3-observer-pattern/
+
+using System.Collections.Generic;
+
+//Invokes the notificaton method
+public class Subject
+{
+    //A list with observers that are waiting for something to happen
+    List<Observer> observers = new List<Observer>();
+
+    //Send notifications if something has happened
+    public void Notify()
+    {
+        for (int i = 0; i < observers.Count; i++)
+        {
+            //Notify all observers even though some may not be interested in what has happened
+            //Each observer should check if it is interested in this event
+            observers[i].OnNotify();
+        }
+    }
+
+    //Add observer to the list
+    public void AddObserver(Observer observer)
+    {
+        observers.Add(observer);
+    }
+
+    //Remove observer from the list
+    public void RemoveObserver(Observer observer)
+    {
+    }
+}
