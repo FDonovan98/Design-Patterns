5b2138f244481d82d072f04e75d9ff5b1ea598a0
HDonovan96 <harry.bd11@gmail.com>
2020-03-11 10:41:17 +0000

    Added initial code for input handler and blank files for other scripts

---
+++ Assets/Scripts/EventDriven/AgentInputHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AgentInputHandler : MonoBehaviour
+{
+    [SerializeField]
+    private CommandObject[] commandList;
+
+    private void Update()
+    {
+        foreach (CommandObject element in commandList)
+        {
+            if (Input.GetKeyDown(element.Keycode))
+            {
+                element.Execute(this);
+            }
+        }
+    }
+}
