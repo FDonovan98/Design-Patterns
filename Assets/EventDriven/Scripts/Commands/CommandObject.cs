6700cbee6226e60a099cbebe9e1489351344faed
HDonovan96 <harry.bd11@gmail.com>
2020-03-15 13:09:14 +0000

    Added ability to rebind keys from keybind menu

--- Assets/EventDriven/Scripts/Commands/CommandObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class CommandObject : ScriptableObject
-{
-    [SerializeField]
-    public KeyCode keycode;
-
-    public abstract void Execute(GameObject agent, MovementValues movementValues);
-}
